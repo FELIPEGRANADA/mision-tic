6fddc029ae091e26b89adbda40857350f9d8fb46
Linamcaro <lina.mcaro@gmail.com>
2021-10-12 20:36:38 -0500

    adding auth0

--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
+
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId =  process.env.REACT_APP_AUTH0_CLIENTID;
+
 
 // Application Entry point 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider 
+      domain = {domain}
+      clientId={clientId}
+      redirectUri = {window.location.origin}
+      >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
